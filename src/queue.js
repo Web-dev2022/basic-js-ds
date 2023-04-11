9f7ce49939f08dcef76df1c9da139c33cbbb6fba
Stanislav Cherevkov <web--dev@hotmail.com>
2023-04-11 18:13:37 +0300

    feat: add queue functional

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+ class ListNode {
+  constructor(value) {
+    this.value = value; 
+    this.next = null;
+  }
+}
+
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.storage = [];
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    const result = new ListNode(this.storage[0]);
+    result.next = new ListNode(this.storage[1]);
+
+    let currentNode = result.next;
+
+    for (let i = 2; i < this.storage.length; i++ ) {
+      currentNode.next = new ListNode(this.storage[i]);
+      currentNode = currentNode.next;
+    }
+
+    return result;
+  }
+
+  enqueue(value) {
+    this.storage.push(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.storage[0];
+    this.storage.shift();
+
+    return result;
   }
 }
 
